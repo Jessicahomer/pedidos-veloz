008b94801afded6f920917b730d40047f57ad20f
Jéssica <jessicahomercb@gmail.com>
2026-08-07 20:42:39 -0300

    Corrige proxy do API Gateway

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -3,42 +3,39 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-    res.json({
-        service: "API Gateway",
-        status: "online"
-    });
+  res.json({
+    service: "API Gateway",
+    status: "online"
+  });
 });
 
 app.use(
-    "/pedidos",
-    createProxyMiddleware({
-        target: "http://pedido-service:3001",
-        changeOrigin: true
-    })
+  "/pedidos",
+  createProxyMiddleware({
+    target: "http://pedido-service:3001",
+    changeOrigin: true
+  })
 );
 
 app.use(
-    "/estoque",
-    createProxyMiddleware({
-        target: "http://estoque-service:3002",
-        changeOrigin: true
-    })
+  "/estoque",
+  createProxyMiddleware({
+    target: "http://estoque-service:3002",
+    changeOrigin: true
+  })
 );
 
 app.use(
-    "/pagamento",
-    createProxyMiddleware({
-        target: "http://pagamento-service:3003",
-        changeOrigin: true
-    })
+  "/pagamento",
+  createProxyMiddleware({
+    target: "http://pagamento-service:3003",
+    changeOrigin: true
+  })
 );
-
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`API Gateway rodando na porta ${PORT}`);
+  console.log(`API Gateway rodando na porta ${PORT}`);
 });
